c984d1d5c5d5302e27199e4cb9ebc8d58670fbb9
RaoufAddeche <addeche.raouf@gmail.com>
2025-03-13 16:06:50 +0100

    deploiement effectué izi

--- Simplon_App_bancaire_Django/Bamk/Bamk/settings.py
+++ Simplon_App_bancaire_Django/Bamk/Bamk/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4%tz@cgg0=mtecbkr1@^$$n&$)#$9$m1_y94akyu$n7)9ljqk-'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
